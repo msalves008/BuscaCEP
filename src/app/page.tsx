23030e3be91948ea23443ff9eeff1128e592cf90
msalves008 <matheusdesousaalves2018@gmail.com>
2024-05-01 23:16:51 -0300

    feat: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import AddressCard from './_components/address-info/address-card'
+import { ZipCodeForm } from './_components/search-zip-code/zip-code-form'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <ZipCodeForm />
+      <AddressCard />
+    </main>
+  )
+}
