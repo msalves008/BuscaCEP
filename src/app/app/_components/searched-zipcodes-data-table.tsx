6a887cd15c7ab68d34261db90fe7e3a35c292c7a
msalves008 <matheusdesousaalves2018@gmail.com>
2024-05-04 17:30:02 -0300

    refactor: adding card title and reordering table columns

--- src/app/app/_components/searched-zipcodes-data-table.tsx
+++ src/app/app/_components/searched-zipcodes-data-table.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 
@@ -23,29 +28,30 @@ export function SearchedZipCodesDataTable({
   searchedZipCodes,
 }: SearchedZipCodesDataTableProps) {
   const { push } = useRouter()
-
   return (
     <Card className="p-4 m-4">
+      <CardHeader>
+        <CardTitle>CEPs Pesquisados</CardTitle>
+      </CardHeader>
       <CardContent>
         <Table>
-          <TableCaption>Ceps Pesquisados</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">CEP</TableHead>
-              <TableHead>Estado</TableHead>
-              <TableHead>Cidade</TableHead>
+              <TableHead>Logradouro</TableHead>
               <TableHead>Bairro</TableHead>
-              <TableHead>Rua</TableHead>
+              <TableHead>Cidade</TableHead>
+              <TableHead>Estado</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {searchedZipCodes?.map((item) => (
               <TableRow key={item.cep}>
                 <TableCell className="font-medium">{item.cep}</TableCell>
-                <TableCell>{item.state}</TableCell>
-                <TableCell>{item.city}</TableCell>
-                <TableCell>{item.neighborhood}</TableCell>
                 <TableCell>{item.street}</TableCell>
+                <TableCell>{item.neighborhood}</TableCell>
+                <TableCell>{item.city}</TableCell>
+                <TableCell>{item.state}</TableCell>
               </TableRow>
             ))}
           </TableBody>
